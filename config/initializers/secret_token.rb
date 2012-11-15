0c722347f34bef2d97f5e6645b2c57c34dc65658
Alex Propes <alex.propes@gmail.com>
2012-11-15 13:52:00 -0500

    Initial Build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Educrew::Application.config.secret_token = 'e41042a1a801fbbadd9bb04d3a71c21f9778c85c1eaf9a87ca145f1733145924f3b7ab9bdbe2ddc9d5ee164cdabc419265e0e22c80ebf8533c6002cd0d06eeb2'
